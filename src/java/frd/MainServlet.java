589908a2c6e59849495eb703d6ea1926d20f4fc1
sergioviera <sergioviera@gmail.com>
2015-09-02 21:01:07 -0300

    iniciar repositorio

---
+++ src/java/frd/MainServlet.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frd;
+
+import frd.memento.CareTaker;
+import frd.memento.Originator;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author lsi
+ */
+public class MainServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String action = request.getParameter("save");
+        if( action!=null && !"".equals(action) ){
+            //guardar
+        }
+        
+        Originator originator = new Originator();
+        CareTaker careTaker = new CareTaker();
+
+        originator.setState("State #1");
+        originator.setState("State #2");
+        careTaker.add(originator.saveStateToMemento());
+
+        originator.setState("State #3");
+        careTaker.add(originator.saveStateToMemento());
+
+        String msg;
+        originator.setState("State #4");
+        msg = "Current State: " + originator.getState();		
+
+        originator.getStateFromMemento(careTaker.get(0));
+        msg += "<br>First saved State: " + originator.getState();
+        originator.getStateFromMemento(careTaker.get(1));
+        msg += "<br>Second saved State: " + originator.getState();
+      
+        request.setAttribute("msg", msg);
+        
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : index
+    Created on : 02/09/2015, 19:24:10
+    Author     : lsi
+--%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ page import="java.util.List" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Memento Project</title>
+    </head>
+    <body>
+        <h1>Patron Memento</h1>
+        
+        <%=
+           request.getAttribute("msg")
+        %>
+        
+    </body>
+</html>
